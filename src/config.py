4d628658d2b5bac7a0fef0d693d56d27f44f672a
hoss <hoss@hoss-dev.home>
2025-10-17 13:37:03 +0100

    finalise config for both local and Lambda environment configuration

--- src/config.py
+++ src/config.py
@@ -1,16 +1,22 @@
-# src/config.py
+# src/config.py ( for local and Lambda environment configuration)
 
 import os
 from dotenv import load_dotenv
 
-# Load variables from the .env file into the environment
+# This will load the .env file ONLY when running locally  with Lambda, it does nothing
+
 load_dotenv()
 
 # --- Guardian API Configuration ---
-# Safely get the API key from the environment variables
+# On Lambda, this will be set as an environment variable by Terraform from terraform.tfvars
+# Locally, it will be loaded from the .env file.
 API_KEY = os.getenv('API_KEY')
 BASE_URL = 'https://content.guardianapis.com/search'
 
-# A basic check to ensure the API key is loaded.
+# --- AWS Configuration ---
+# These will also be set by Terraform on Lambda environment
+AWS_REGION = os.getenv('AWS_REGION', 'eu-west-2') # Default to eu-west-2 ( hard coded unless provided otherwise)
+KINESIS_STREAM_NAME = os.getenv('KINESIS_STREAM_NAME', 'guardian_content') # Default to guardian_content ( set in main.tf)
+
 if not API_KEY:
-    raise ValueError("API_KEY not found. Please ensure it is set in your .env file.")
+    raise ValueError("API_KEY not found. Please set it in your .env file or Lambda environment variables.")
